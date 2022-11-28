bbd22a812744c228ca5784f2b1a54364c55d7a23
Jão Carlos <123jccml@.com>
2022-11-27 21:56:43 -0300

    :tada: Init Project

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'Pages/to_do_list_page.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ToDoListPage(),
+    );
+  }
+}
