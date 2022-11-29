361b05290269cc2a1b1348130014880d05b4fbcc
Jão Carlos <123jccml@.com>
2022-11-29 17:20:02 -0300

    :white_check_mark: Add First Page

--- lib/Pages/to_do_list_page.dart
+++ lib/Pages/to_do_list_page.dart
@@ -1,10 +1,39 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
 class ToDoListPage extends StatelessWidget {
-  const ToDoListPage({super.key});
+  ToDoListPage({super.key});
+
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.redAccent,);
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'E-mail: ',
+                ),
+              ),
+              ElevatedButton(
+                onPressed: login,
+                child: const Text('Entrar'),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void login() {
+    String email = emailController.text;
+    print(email);
   }
 }
